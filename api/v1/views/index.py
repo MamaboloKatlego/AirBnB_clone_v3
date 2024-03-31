91b5ad1b8dd8b94c0824d6a7dd75d800599f67fd
MamaboloKatlego <132917857+MamaboloKatlego@users.noreply.github.com>
2024-03-31 08:43:47 +0200

    Update index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,35 +1,26 @@
-#!/usr/bin/python3
-"""
-Module docs
-"""
 from api.v1.views import app_views
 from flask import jsonify
-from models import storage
 
-
+# create a route /status on the object app_views that returns a JSON: "status": "OK"
 @app_views.route('/status', strict_slashes=False)
-def health_check():
-    """
-    Function Docs
-    """
+def status():
+    """ Method to return status """
     return jsonify({"status": "OK"})
 
 
-@app_views.route("/stats")
-def count():
-    """
-    Function Docs
-    """
-    objs = {
-            'amenities': storage.count('Amenity'),
-            'cities': storage.count('City'),
-            'places': storage.count('Place'),
-            'reviews': storage.count('Review'),
-            'states': storage.count('State'),
-            'users': storage.count('User')
-            }
-    return jsonify(objs)
+"""Create an endpoint that retrieves the number of each objects by type:
 
+In api/v1/views/index.py
+Route: /api/v1/stats
+You must use the newly added count() method from storage"""
 
-if __name__ == "__main__":
-    pass
+@app_views.route('/stats', strict_slashes=False)
+def stats():
+    """ Method to return stats """
+    from models import storage
+    return jsonify({"amenities": storage.count("Amenity"),
+                    "cities": storage.count("City"),
+                    "places": storage.count("Place"),
+                    "reviews": storage.count("Review"),
+                    "states": storage.count("State"),
+                    "users": storage.count("User")})
